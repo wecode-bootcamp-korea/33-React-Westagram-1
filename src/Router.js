4f41a705e7c0c605f8db4c67b577a0bd73b7b8b1
hyeonjoo KIM <hyeonjookim@hyeonjoos-MacBook-Pro.local>
2022-05-11 16:26:23 +0900

    Modify: first feedback

--- src/Router.js
+++ src/Router.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // 김현주
-import LoginHj from "./pages/hyeonjooKim/Login/LoginHj";
-import MainHj from "./pages/hyeonjooKim/Main/MainHj";
+import LoginHj from './pages/hyeonjooKim/Login/LoginHj';
+import MainHj from './pages/hyeonjooKim/Main/MainHj';
 
 // 노해리
-import LoginHr from "./pages/haeriRoh/Login/LoginHr";
-import MainHr from "./pages/haeriRoh/Main/MainHr";
+import LoginHr from './pages/haeriRoh/Login/LoginHr';
+import MainHr from './pages/haeriRoh/Main/MainHr';
 
 // 윤경연
-import LoginKy from "./pages/kyeongyeon/Login/LoginKy";
-import MainKy from "./pages/kyeongyeon/Main/MainKy";
+import LoginKy from './pages/kyeongyeon/Login/LoginKy';
+import MainKy from './pages/kyeongyeon/Main/MainKy';
 
 // 김슬비
-import LoginSb from "./pages/seulbiKim/Login/LoginSb";
-import MainSb from "./pages/seulbiKim/Main/MainSb";
+import LoginSb from './pages/seulbiKim/Login/LoginSb';
+import MainSb from './pages/seulbiKim/Main/MainSb';
 
 // 안수정
-import LoginSj from "./pages/soojungAn/Login/LoginSj";
-import MainSj from "./pages/soojungAn/Main/MainSj";
+import LoginSj from './pages/soojungAn/Login/LoginSj';
+import MainSj from './pages/soojungAn/Main/MainSj';
 
 const Router = () => {
   return (
