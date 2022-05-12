c1fe0929bc1124bc4642a0df8c45a0a22c45438b
hyeonjoo KIM <hyeonjookim@hyeonjoos-MacBook-Pro.local>
2022-05-12 15:23:45 +0900

    ADD: Nav Component

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,24 @@
+import './Nav.scss';
+
+const Nav = () => {
+  return (
+    <nav className="nav-container">
+      <ul className="nav-list">
+        <li>
+          <i className="fa-brands fa-instagram"></i>
+          <span className="nav-logo-letter">Westagram</span>
+        </li>
+        <li>
+          <input className="nav-input" type="text" placeholder="검색" />
+        </li>
+        <li className="nav-icon-area">
+          <i className="fa-regular fa-compass" />
+          <i className="fa-regular fa-heart" />
+          <i className="fa-regular fa-user" />
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Router from './Router';
+// import Router from './Router';
+import Nav from './components/Nav/Nav';
 import './styles/common.scss';
 import './styles/reset.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router />
+    {/* <Router /> */}
+    <Nav />
   </React.StrictMode>
 );
