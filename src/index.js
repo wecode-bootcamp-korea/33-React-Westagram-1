3a3ba72cd6cf46c1ac09a3f6da59f84d1cbd7b23
hyeonjoo KIM <hyeonjookim@hyeonjoos-MacBook-Pro.local>
2022-05-12 15:32:51 +0900

    Modify: Nav Component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import Router from './Router';
-import Nav from './components/Nav/Nav';
+import Router from './Router';
 import './styles/common.scss';
 import './styles/reset.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Router /> */}
-    <Nav />
+    <Router />
   </React.StrictMode>
 );
