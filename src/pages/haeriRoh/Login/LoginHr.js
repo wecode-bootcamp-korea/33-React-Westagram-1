737bf1f472b4f43ac9a623c7fbfbbc0bf192557e
yesharry <reerohh@gmail.com>
2022-05-13 15:53:35 +0900

    modify: scss 카멜케이스

--- src/pages/haeriRoh/Login/LoginHr.js
+++ src/pages/haeriRoh/Login/LoginHr.js
@@ -1,7 +1,57 @@
 import React from 'react';
+import './LoginHr.scss';
+import { Link, useNavigate } from 'react-router-dom';
 
-function Login() {
-  return <div>Login</div>;
-}
+const LoginHr = () => {
+  const navigate = useNavigate();
 
-export default Login;
+  const goToMain = () => {
+    navigate('/MainHr');
+  };
+
+  return (
+    <div>
+      <div className="main">
+        <div className="container">
+          <div className="lineBox">
+            <div className="contentsBox">
+              <div className="logo">
+                <span className="logoName">Westagram</span>
+              </div>
+              <div className="loginBox">
+                <div className="inputs">
+                  <div className="input1">
+                    <input
+                      type="text"
+                      className="id"
+                      placeholder="전화번호, 사용자 이름 또는 이메일"
+                    />
+                  </div>
+                  <div className="input1">
+                    <input
+                      type="password"
+                      className="pw"
+                      placeholder="비밀번호"
+                    />
+                  </div>
+                </div>
+                <div className="btnBox">
+                  <button className="loginBtn" type="button" onClick={goToMain}>
+                    로그인
+                  </button>
+                </div>
+              </div>
+              <div className="findBox">
+                <a href="" className="findPw">
+                  비밀번호를 잊으셨나요?
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginHr;
--- src/pages/haeriRoh/Main/MainHr.js
+++ src/pages/haeriRoh/Main/MainHr.js
@@ -1,7 +1,208 @@
 import React from 'react';
+import './MainHr.scss';
 
-function Main() {
-  return <div>Main</div>;
-}
+const MainHr = () => {
+  return (
+    <div>
+      <div className="insta">
+        <div className="container">
+          <nav>
+            <div className="rightNav">
+              <div className="logo">
+                <i className="fa-brands fa-instagram"></i>
+              </div>
+              <div className="bar"></div>
+              <div className="nameLogo">
+                <span>Westagram</span>
+              </div>
+            </div>
+            <div className="midNav">
+              <input className="search" placeholder="검색" />
+            </div>
+            <div className="leftNav">
+              <div className="compass">
+                <i className="fa-regular fa-compass"></i>
+              </div>
+              <div className="heart">
+                <i className="fa-regular fa-heart"></i>
+              </div>
+              <div className="profile">
+                <i className="fa-regular fa-user"></i>
+              </div>
+            </div>
+          </nav>
+          <main>
+            <div className="feeds">
+              <div className="container">
+                <article>
+                  <div className="feedBox">
+                    <div className="topFeed">
+                      <div className="idBox">
+                        <div className="profilePic"></div>
+                        <span className="instaId">yesharryy</span>
+                      </div>
+                      <div className="more">
+                        <i className="fa-solid fa-ellipsis"></i>
+                      </div>
+                    </div>
+                    <div className="midFeed"></div>
+                    <div className="bottomFeed">
+                      <div className="iconBottom">
+                        <div className="rightIconBottom">
+                          <i className="fa-solid fa-heart"></i>
+                          <i className="fa-regular fa-comment"></i>
+                          <i className="fa-regular fa-share-from-square"></i>
+                        </div>
+                        <div className="leftIconBottom">
+                          <i className="fa-regular fa-bookmark"></i>
+                        </div>
+                      </div>
+                      <div className="likeBottom">
+                        <div className="likeBox">
+                          <div className="likePic"></div>
+                          <span className="likeCount">
+                            <b>yesharryy</b>님 외 <b>10명</b>이 좋아합니다
+                          </span>
+                        </div>
+                      </div>
+                      <div className="commentBox">
+                        <div className="myComment">
+                          <b>yesharryy</b> 김우주 보고싶어~!!
+                        </div>
+                        <div className="otherComment">
+                          <p id="todoList"></p>
+                          <form id="todoForm">
+                            <input
+                              type="text"
+                              className="msg"
+                              placeholder=" 댓글 달기..."
+                              required
+                            />
+                          </form>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </article>
+              </div>
+              <div className="mainRight">
+                <div className="rightBox">
+                  <div className="myId">
+                    <div className="myPic"></div>
+                    <div className="myIdBox">
+                      <span className="myIdName">
+                        <b>yesharryy</b>
+                      </span>
+                      <span className="myIdInfo">집 가고싶다 | 노해리</span>
+                    </div>
+                  </div>
+                  <div className="story">
+                    <div className="topStory">
+                      <span className="storyText">스토리</span>
+                      <span className="moreText">모두 보기</span>
+                    </div>
+                    <div className="storyBox">
+                      <div className="storyPic"></div>
+                      <div className="storyInfo">
+                        <div className="storyId">
+                          <b>westagram</b>
+                        </div>
+                        <div className="storyTime">16분 전</div>
+                      </div>
+                    </div>
+                    <div className="storyBox">
+                      <div className="storyPic"></div>
+                      <div className="storyInfo">
+                        <div className="storyId">
+                          <b>westagram</b>
+                        </div>
+                        <div className="storyTime">16분 전</div>
+                      </div>
+                    </div>
+                    <div className="storyBox">
+                      <div className="storyPic"></div>
+                      <div className="storyInfo">
+                        <div className="storyId">
+                          <b>westagram</b>
+                        </div>
+                        <div className="storyTime">16분 전</div>
+                      </div>
+                    </div>
+                    <div className="storyBox">
+                      <div className="storyPic"></div>
+                      <div className="storyInfo">
+                        <div className="storyId">
+                          <b>westagram</b>
+                        </div>
+                        <div className="storyTime">16분 전</div>
+                      </div>
+                    </div>
+                    <div className="storyBox">
+                      <div className="storyPic"></div>
+                      <div className="storyInfo">
+                        <div className="storyId">
+                          <b>westagram</b>
+                        </div>
+                        <div className="storyTime">16분 전</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="recommand">
+                    <div className="topStory">
+                      <span className="storyText">회원님을 위한 추천</span>
+                      <span className="moreText">모두 보기</span>
+                    </div>
+                    <div className="reBox">
+                      <div className="reIdBox">
+                        <div className="rePic"></div>
+                        <div className="reInfo">
+                          <div className="reId">
+                            <b>westagram</b>
+                          </div>
+                          <div className="reFriends">16분 전</div>
+                        </div>
+                      </div>
+                      <div className="follow">팔로우</div>
+                    </div>
+                    <div className="reBox">
+                      <div className="reIdBox">
+                        <div className="rePic"></div>
+                        <div className="reInfo">
+                          <div className="reId">
+                            <b>westagram</b>
+                          </div>
+                          <div className="reFriends">16분 전</div>
+                        </div>
+                      </div>
+                      <div className="follow">팔로우</div>
+                    </div>
+                    <div className="reBox">
+                      <div className="reIdBox">
+                        <div className="rePic"></div>
+                        <div className="reInfo">
+                          <div className="reId">
+                            <b>westagram</b>
+                          </div>
+                          <div className="reFriends">16분 전</div>
+                        </div>
+                      </div>
+                      <div className="follow">팔로우</div>
+                    </div>
+                  </div>
+                  <div className="instaInfo">
+                    Instagram 정보 ・ 지원 ・ 홍보 센터 ・ API ・ <br />
+                    채용 정보 ・ 개인정보처리방침 ・ 약관 ・ <br />
+                    디렉터리 ・ 프로필 ・ 해시태그 ・ 언어 <br />
+                    <br />Ⓒ 2022 INSTAGRAM
+                  </div>
+                </div>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Main;
+export default MainHr;
