b49a977ef40593f40ffa72d8325c398fde4811fb
Yuntroll95 <yuntrollpark@gamail.com>
2022-05-14 14:31:36 +0900

    Add: Mission 2: 로그인 기능 구현완료

--- src/pages/kyeongyeon/Login/LoginKy.js
+++ src/pages/kyeongyeon/Login/LoginKy.js
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
 import './LoginKy.scss';
 
 const LoginKy = () => {
@@ -8,18 +9,29 @@ const LoginKy = () => {
     navigate('/main');
   };
 
+  const [inputId, setInputId] = useState(''); //useState를 사용해서 변하는 값을 set함수로 받음
+  const [inputPw, setInputPw] = useState('');
+
+  const handleInputId = e => {
+    // input data에 변화가 있을때마다 value값을 변경해서 useState해줌
+    setInputId(e.target.value);
+  };
+
+  const handleInputPw = e => {
+    setInputPw(e.target.value);
+  };
+
   return (
     <div className="login">
-      s
       <section>
         <div className="loginPage">
           <div className="boxInstagram">
             <h1>Westagram</h1>
             <div className="userID">
               <input
+                onChange={handleInputId}
                 type="text"
-                id="idInput"
-                value=""
+                value={inputId}
                 title="사용자 입력"
                 placeholder="전화번호, 사용자 이름 또는 이메일"
               />
@@ -27,9 +39,9 @@ const LoginKy = () => {
 
             <div className="userPW">
               <input
+                onChange={handleInputPw}
                 type="password"
-                id="pwInput"
-                value=""
+                value={inputPw}
                 title="사용자 입력"
                 placeholder="비밀번호"
               />
@@ -37,14 +49,17 @@ const LoginKy = () => {
 
             <div className="btnLogin">
               <button
+                onClick={goToMain}
                 type="button"
                 id="activeButton"
                 onclick=" location.href = 'mainpage.html'"
-                disabled
+                disabled={
+                  inputId.includes('@') && inputPw.length > 4 ? false : true
+                }
               >
                 로그인
               </button>
-              <button onClick={goToMain}>메인페이지로 이동하기</button>
+              {/* <button onClick={goToMain}>메인페이지로 이동하기</button> */}
             </div>
 
             <p>
