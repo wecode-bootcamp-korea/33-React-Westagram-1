d709457747192a62121c5f0210ce5866ce85b9f3
hyeonjoo KIM <hyeonjookim@hyeonjoos-MacBook-Pro.local>
2022-05-17 02:16:51 +0900

    Modify: LoginHJ 컴포넌트 useEffect 적용

--- src/pages/hyeonjooKim/Login/LoginInput.js
+++ src/pages/hyeonjooKim/Login/LoginInput.js
@@ -2,13 +2,17 @@ import './LoginInput.scss';
 
 const LoginInput = ({ onChangeInput, type, name, placeholder }) => {
   const handledInput = event => {
-    onChangeInput(event.target.name, event.target.value);
+    onChangeInput(event.target.value);
   };
+
+  // const handledInput = event => {
+  //   onChangeInput(event.target.name, event.target.value);
+  // };
 
   return (
     <input
       type={type}
-      name={name}
+      // name={name}
       placeholder={placeholder}
       onChange={handledInput}
     />
